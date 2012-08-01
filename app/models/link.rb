5f594df67c4490affe9f6fa9d01bb66f9235368f
Liam McArdle <liam@catholicsteward.com>
2012-07-31 20:40:27 -0700

    callback for undesirable words

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,8 @@ class Link < ActiveRecord::Base
   attr_accessible :original_url, :short_url, :user_id
   belongs_to :user
   
+  before_save { self.short_url.downcase! }
+  before_save :undesirable_words
   before_save :vanity_url_check
   before_save :clean_up_url
   
@@ -9,6 +11,15 @@ class Link < ActiveRecord::Base
   
   private
   
+    def undesirable_words
+      undesirable_words = ["hate", "bad", "fuck", "shit", "bitch"]
+      if undesirable_words.include?(self.short_url)
+        self.short_url = ""
+      else
+        self.short_url = self.short_url
+      end
+    end
+    
     def vanity_url_check
       if self.short_url.empty?
         random_string = (0...8).map{65.+(rand(25)).chr}.join
